36f9552800e24b5ebcdaf6ebe4e6411cea42114c
Artem Prokop <artem13.prokop@gmail.com>
2025-12-01 19:55:08 +0100

    chore: smaller error format

--- src/middlewares/base.ts
+++ src/middlewares/base.ts
@@ -1,35 +1,35 @@
-import { jwt as jwtPlugin } from '@elysiajs/jwt'
-import { Elysia, t } from 'elysia'
+import { jwt as jwtPlugin } from '@elysiajs/jwt';
+import { Elysia, t } from 'elysia';
 
-import cors from '@elysiajs/cors'
-import { createDbConnection } from '../services/database'
+import cors from '@elysiajs/cors';
+import { createDbConnection } from '../services/database';
 
 export const RequireBase = new Elysia({ name: 'Middleware.Base' })
-	.use(
-		cors({
-			origin: true,
-			credentials: true,
-			allowedHeaders: ['Content-Type', 'Authorization']
-		})
-	)
-	.use(
-		jwtPlugin({
-			name: 'jwt',
-			// FIXME: Move to env
-			secret: 'Shirbity Shnorble',
-			schema: t.Object({
-				userId: t.String()
-			})
-		})
-	)
-	.on('error', ({ error }) => {
-		console.error(error)
-		return { ok: false, data: null, error: error.message ?? 'Unknown error' }
-	})
-	.decorate(
-		'database', //
-		await createDbConnection(
-			process.env.DB_CONNECTION_URL ?? 'mongodb://127.0.0.1:27017'
-		)
-	)
-	.as('plugin')
+  .use(
+    cors({
+      origin: true,
+      credentials: true,
+      allowedHeaders: ['Content-Type', 'Authorization'],
+    }),
+  )
+  .use(
+    jwtPlugin({
+      name: 'jwt',
+      // FIXME: Move to env
+      secret: 'Shirbity Shnorble',
+      schema: t.Object({
+        userId: t.String(),
+      }),
+    }),
+  )
+  .on('error', ({ error }) => {
+    if (error.message !== 'Unauthorized') {
+      console.error(error);
+    }
+    return { ok: false, data: null, error: error.message ?? 'Unknown error' };
+  })
+  .decorate(
+    'database', //
+    await createDbConnection(process.env.DB_CONNECTION_URL ?? 'mongodb://127.0.0.1:27017'),
+  )
+  .as('plugin');
